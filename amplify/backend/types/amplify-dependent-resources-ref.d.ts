2b3ca92f2313ee250e4094b2aff827f62f688e4c
victorbern <victorbernardo96@gmail.com>
2022-06-17 20:28:54 -0300

    Novo commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp1c8b5e80": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { I18n } from 'aws-amplify';
+import AmplifyI18n from 'amplify-i18n';
+Amplify.configure(config);
+const locales = ["en", "fr", "pt-BR"]
+AmplifyI18n.configure(locales)
+I18n.setLanguage("pt-BR")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
